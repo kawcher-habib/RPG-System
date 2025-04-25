7f5289d3976ff7582014a55ae9703fed2b68fc52
kawcher habib <kawcher578@gmail.com>
2025-04-25 09:50:55 +0600

    auth controller add

---
+++ controllers/auth/AuthController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace controllers\auth\AuthController;
+
+/**
+ * All about business logic here
+ *   1. 
+ *   1. Validation
+ * 
+ */
+class AuthController{
+
+    /**
+     * 
+     * 
+     * @param mixed $requestBody
+     * @return void
+     */
+    public function registration($requestBody){
+
+    /**
+     *  TODO: 1. validation checker
+     *          *. Input validation  [Done];
+     *          *. IsUser Exist?
+     *
+     */
+
+
+    if (empty((array) $requestBody)) {
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "Json Body is Empty"
+        ]);
+
+        return;
+
+    }
+    
+    
+    if (empty($requestBody->fname)) {
+
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "First name require"
+        ]);
+
+        return;
+    } else if (empty($requestBody->lname)) {
+
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "Last name require"
+        ]);
+
+        return;
+    } else if (empty($requestBody->email)) {
+
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "Email require"
+        ]);
+
+        return;
+
+    } else if (empty($requestBody->password)) {
+
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "Password require"
+        ]);
+
+        return;
+    } else if (empty($requestBody->role)) {
+
+        echo json_encode([
+            "status" => "Failed",
+            "message" => "Role require"
+        ]);
+
+        return;
+    }
+
+        /** TODO:
+         *  1. fullFeel all condition
+         *  2. connection with models
+         */
+
+
+    }
+
+    
+
+    public function login($requestBody){
+
+    }
+}
+
+
+
+
+
--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,7 @@
 <?php
 
-
-
+ use controllers\auth\AuthController\AuthController;
+include_once('../controllers/auth/AuthController.php');
 /**
  *  User Management [API]
  * 
@@ -9,74 +9,16 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 $prefix = $_GET['prefix']; /** TODO: if prefix is empty? */
+$auth = new AuthController();
 
 if ($method == 'GET' && $prefix == 'login') {
 
     echo json_encode(['message' => "$prefix"]); // Test
 
 } else if ($method == 'POST' && $prefix = 'registration') {
-    /**
-     *  TODO: 1. validation checker
-     *          *. Input validation  [Done];
-     *          *. IsUser Exist?
-     *
-     */
 
-    $data = json_decode(file_get_contents('php://input'), true);
+    $requestBody = json_decode(file_get_contents('php://input'), true);
 
-    if (empty((array) $data)) {
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "Json Body is Empty"
-        ]);
-
-        return;
-
-    }
-
-    if (empty($data->fname)) {
-
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "First name require"
-        ]);
-
-        return;
-    } else if (empty($data->lname)) {
-
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "Last name require"
-        ]);
-
-        return;
-    } else if (empty($data->email)) {
-
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "Email require"
-        ]);
-
-        return;
-
-    } else if (empty($data->password)) {
-
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "Password require"
-        ]);
-
-        return;
-    } else if (empty($data->role)) {
-
-        echo json_encode([
-            "status" => "Failed",
-            "message" => "Role require"
-        ]);
-
-        return;
-    }
-
-    print_r($data);
+    $auth->registration($requestBody);
 
 }
